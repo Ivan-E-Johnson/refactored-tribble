0d81143fc08bdd89fd155ba4bf86f6b0677d54f6
Ivan Johnson <iejohnson@uiowa.edu>
2023-10-04 18:48:52 -0500

    ENH: Fix probe

--- readTemp.py
+++ readTemp.py
@@ -6,17 +6,9 @@ import time
 
 
 
-# Load the necessary kernel modules to read the temperature from thermocouple
-os.system('modprobe w1-gpio')  # load kernel module for GPIO interface of 1-wire bus
-os.system('modprobe w1-therm')  # load kernel module for temperature measurement via 1-wire bus
 device_file = '/sys/bus/w1/devices/28-3c80e3819f89/w1_slave'  # path to the device file that stores temperature data
 
 
-# Create a connection object to the SQLite database 'temperature.db'
-global conn, cursor
-conn = sqlite3.connect('temperature.db')
-# Create a cursor object to execute SQL queries
-cursor = conn.cursor()
 
 # Function to read the raw temperature data from the device file
 def read_temp_raw():
@@ -35,8 +27,6 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
-        cursor.execute("INSERT INTO temperature (temp_c) VALUES (?);", (temp_c,))
-        conn.commit()
 
         return temp_c
 
