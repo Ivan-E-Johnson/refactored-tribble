104a3efd20e7fdc2e0da346f9af57343974cdf2d
Ivan Johnson <iejohnson@uiowa.edu>
2023-10-04 17:30:28 -0500

    ENH: Better implementation for extensibility

--- dht22.py
+++ dht22.py
@@ -18,19 +18,33 @@ def insert_data(temperature, humidity):
     except Exception as e:
         print(f"Error inserting data into the database: {str(e)}")
 
-try:
-    while True:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-        if humidity is not None and temperature is not None:
-            print(f"Temperature: {temperature:.2f}°C, Humidity: {humidity:.2f}%")
-            
-            # Insert data into the database
-            insert_data(temperature, humidity)
-            
-        else:
-            print("Failed to retrieve data from DHT sensor")
-        time.sleep(2)  # Read data every 2 seconds
+def setup_DHT():
+    # DHT22 sensor setup
+    DHT_SENSOR = Adafruit_DHT.DHT22
+    DHT_PIN = 17  # Change this to match your GPIO pin
+    return DHT_SENSOR, DHT_PIN # is this useful?
 
-except KeyboardInterrupt:
-    pass
+
+# Function to read sensor data
+def read_sensor_data(DHT_SENSOR,DHT_PIN):
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    return humidity, temperature
+
+if __name__ == '__main__':
+    try:
+        DHT_SENSOR, DHT_PIN = setup_DHT()
+        while True:
+            humidity, temperature = read_sensor_data(DHT_SENSOR, DHT_PIN) 
+            if humidity is not None and temperature is not None:
+                print(f"Temperature: {temperature:.2f}°C, Humidity: {humidity:.2f}%")
+            
+                # Insert data into the database
+                # insert_data(temperature, humidity)
+            
+            else:
+                print("Failed to retrieve data from DHT sensor")
+            time.sleep(2)  # Read data every 2 seconds
+
+    except KeyboardInterrupt:
+        pass
 
